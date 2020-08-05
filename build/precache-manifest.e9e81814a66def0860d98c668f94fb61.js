ffde19a542a15293456685be947691fff6f79bc1
mac <joyce@hawkmanlabs.com>
2020-08-05 22:25:52 +0000

    build script
    npm run build

---
+++ build/precache-manifest.e9e81814a66def0860d98c668f94fb61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c50d948a8f47a51401c705775add8c82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be6f07ffa11c684d58ce",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ca921d7cfa58f6da8cd4",
+    "url": "/static/js/2.77b593e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.77b593e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be6f07ffa11c684d58ce",
+    "url": "/static/js/main.75188d6a.chunk.js"
+  },
+  {
+    "revision": "71a059e0deba638031ac",
+    "url": "/static/js/runtime-main.6dcd4189.js"
+  }
+]);
