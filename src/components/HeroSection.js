7e22b6b857f56858eca23a23fdcf682c15fd3e11
mac <joyce@hawkmanlabs.com>
2020-08-20 13:37:19 +0000

    Update HeroSection.js
    router dom change from a href tag

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,26 +1,17 @@
 import React from 'react'
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route
-    
-  } from "react-router-dom";
+
 
 export default function HeroSection() {
     return (
-        <Router>
+        
         <div className="hero row">
         <div className="hero-right col-sm-6 col-sm-6">
             <h1 className="header-headline bold"> Beautiful Free Nova template <br /></h1>
             <h4 className="header-running-text light"> A top notch premium quality template for your next
                 web project.</h4>
-                <Switch>
-                <Route path='/privacy-policy'
-                 component={() => { window.location.href = 'https://example.com/1234';
-                  return null; }}>
-                <button className="hero-btn"> Download FREE!</button>
-            </Route>
-            </Switch>
+               <a href="#" 
+                 className="btn hero-btn"> Download FREE!
+                 </a>
         </div>
 
         <div className="col-sm-6 col-sm-6 ipad">
@@ -30,6 +21,6 @@ export default function HeroSection() {
         <div><img className="mouse" src="assets/images/mouse.svg" alt=""/></div>
 
     </div>
-    </Router>
+    
     )
 }
